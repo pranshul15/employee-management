7513639e2f55390b2e8bdbd2e7e9c4ca3d68f9b2
Pranshul Hatwar <pranshul.hatwar@gmail.com>
2023-11-09 13:36:09 +0530

    Employee Entity

---
+++ src/main/java/com/project/employee/model/Employee.java
@@ -0,0 +1,52 @@
+package com.project.employee.model;
+
+
+public class Employee {
+    public Integer id;
+    public String fname;
+    public String lname;
+    public String email;
+
+    public Employee(Integer id, String fname, String lname, String email) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+    }
+
+    public Employee() {
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+    public String getLname() {
+        return lname;
+    }
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", fname=" + fname + ", lname=" + lname + ", email=" + email + "]";
+    }
+    
+}
